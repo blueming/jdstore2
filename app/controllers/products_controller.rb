1bf29a6b91ca10b3012713f55fdafb5075260c36
blueming <wenyiff66@gmail.com>
2017-05-08 22:18:20 +0800

    implement current_cart function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,13 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def add_to_cart
+    @product = Product.find(params[:id])
+    current_cart.add_product_to_cart(@product)
+
+    # current_cart.add_product_to_cart(@product) 多加一行竟然有双倍的效果，有意思
+    flash[:notice] = "成功加入购物车"
+    redirect_to :back
+  end
 end
